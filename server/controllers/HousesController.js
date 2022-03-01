3498494bd54f8470c8262b1bf590d6a35bacf3ff
Isaiah <isaiah.leiva@gmail.com>
2022-03-01 16:51:21 -0700

    commit

--- server/controllers/HousesController.js
+++ server/controllers/HousesController.js
@@ -1,3 +1,4 @@
+import { housesService } from "../services/HousesService"
 
 
 
@@ -15,11 +16,52 @@ export class HousesController extends BaseController {
 
     async getAll(req, res, next) {
         try {
-            const houses = await
+            const houses = await housesService.getAll(req.query)
+            return res.send(houses)
         } catch (error) {
-
+            next(error)
         }
     }
 
+    async getById(req, res, next) {
+        try {
+            const house = await housesService.getById(req.params.id)
+            return res.send(house)
+        } catch (error) {
+            next(error)
+        }
+    }
 
+    async create(req, res, next) {
+        try {
+            req.body.creatorId = req.userInfo.id
+            const house = await housesService.create(req.body)
+            return res.send(house)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async edit(req, res, next) {
+        try {
+            req.body.creatorId = req.userInfo.id
+            //NOTE: why params below?
+            req.body.id = req.params.id
+            const update = await housesService.edit(req.body)
+            return res.send(update)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async remove(req, res, next) {
+        try {
+            const userID = req.userID.id
+            const houseID = req.params.id
+            await housesService.remove(houseID, userID)
+            return res.send('House Gone Now')
+        } catch (error) {
+            next(error)
+        }
+    }
 }
