430d4e0337ae0d29ab6fbc0cd8410a35c9ecd6a9
Isaiah <isaiah.leiva@gmail.com>
2022-03-01 18:03:20 -0700

    commit

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -7,7 +7,7 @@ class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
-  Houses = mongoose.model('House', HouseSchema)
+  Houses = mongoose.model('House', HouseSchema);
 }
 
 export const dbContext = new DbContext()
